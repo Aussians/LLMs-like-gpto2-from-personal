7098bb1878c02989b3acd8f7db9f486c82bb61e1
罗昊彬 <fmx@something.com>
2025-08-13 12:26:44 +0800

    first commit

---
+++ kv_cache/generate.py
@@ -0,0 +1,26 @@
+
+from .utils import KVCache
+import torch
+
+
+def generate_text_simple(model, idx, max_new_tokens, context_size=None, use_cache=True):
+    model.eval()
+    ctx_len = context_size or model.cfg["context_length"]
+
+    with torch.no_grad():
+        if use_cache:
+            cache = KVCache(n_layers=model.cfg["n_layers"])
+            model.reset_kv_cache()
+            logits = model(idx[:, -ctx_len:], cache=cache)
+
+            for _ in range(max_new_tokens):
+                next_idx = logits[:, -1].argmax(dim=-1, keepdim=True)
+                idx = torch.cat([idx, next_idx], dim=1)
+                logits = model(next_idx, cache=cache)
+        else:
+            for _ in range(max_new_tokens):
+                logits = model(idx[:, -ctx_len:], cache=None)
+                next_idx = logits[:, -1].argmax(dim=-1, keepdim=True)
+                idx = torch.cat([idx, next_idx], dim=1)
+
+    return idx
---
+++ kv_cache_batched/utils.py
@@ -0,0 +1,20 @@
+
+class KVCache:
+    def __init__(self, n_layers, batch_size):
+        self.cache = [
+            [None for _ in range(batch_size)] for _ in range(n_layers)
+        ]
+
+    def get(self, layer_idx, batch_idx):
+        return self.cache[layer_idx][batch_idx]
+
+    def update(self, layer_idx, batch_idx, value):
+        self.cache[layer_idx][batch_idx] = value
+
+    def get_layer(self, layer_idx):
+        return self.cache[layer_idx]
+
+    def reset(self):
+        for layer in self.cache:
+            for i in range(len(layer)):
+                layer[i] = None
